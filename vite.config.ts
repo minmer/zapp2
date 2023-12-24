9bf77d5cb9a375568735bacd3149da2d7b9a2f2a
Michael Mleczek <michalmleczek@outlook.com>
2023-12-24 17:21:05 +0100

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+base: "/zapp2_probe/",
+  plugins: [react()],
+})
