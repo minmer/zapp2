34ab96020e3230b14d795095461019af043a02f4
Michael Mleczek <michalmleczek@outlook.com>
2023-12-24 17:31:01 +0100

    new commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-base: "/zapp2_probe/",
+base: "https://minmer.github.io/zapp2/",
   plugins: [react()],
 })
