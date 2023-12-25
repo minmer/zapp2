332ff9c5ec67a5c24e611700ec6affc138083b78
Michael Mleczek <michalmleczek@outlook.com>
2023-12-26 00:12:08 +0100

    Revert "Update DataFetching.tsx"
    This reverts commit df590e52585b8ef9be96bd9d0992357bde8047e4.

--- src/components/DataFetching.tsx
+++ src/components/DataFetching.tsx
@@ -1,3 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Post {
+    id: number,
+    startDate: Date,
+    endDate: Date,
+    minName: string,
+    minTel: string,
+    priest: string,
+    street: string
+}
+
+const DataFetching: React.FC = () => {
+    const [posts, setPosts] = useState<Post[]>([]);
+    const queryParameters = new URLSearchParams(window.location.search)
+    const kol = queryParameters.get("kol")
+
+    useEffect(() => {
+        axios.get<Post[]>('https://probewebapp20231222201703.azurewebsites.net/api/minkol/list?id=' + kol)
+            .then(res => {
+                console.log(res);
+                setPosts(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
+    // style={{border-collapse: "collapse"}}
+    return (
 
         <div>
             <h2>Kolęda 2023/24</h2>
@@ -6,14 +36,14 @@
             W rozpisce kolędy zostało uwzględnione następujące rzeczy:
             <br />
             1. Ilość punktów wynikających ze służenia
-                < br />
-                2. Staż w służbie liturgicznej(nowi ministranci, ministranci, lektorzy)
-                    < br />
-                    3. Terminy zgłoszone w formularzu
-                        < br />
-                        4. Miejsce zamieszkania(w dniu, w którym macie kolędę, mam nadzieję, że będziecie dostępni)
-                            < br />
-                            Przy rozpisaniu kolędy nie miałem na bieżąco przed sobą listę z imionami, więc przepraszam, jeśli zapomniałem uwzględnić jakieś osobiste prośby.
+            < br />
+            2. Staż w służbie liturgicznej(nowi ministranci, ministranci, lektorzy)
+            < br />
+            3. Terminy zgłoszone w formularzu
+            < br />
+            4. Miejsce zamieszkania(w dniu, w którym macie kolędę, mam nadzieję, że będziecie dostępni)
+            < br />
+            Przy rozpisaniu kolędy nie miałem na bieżąco przed sobą listę z imionami, więc przepraszam, jeśli zapomniałem uwzględnić jakieś osobiste prośby.
             < br />
             <h1 id="name"></h1>
             <br />
@@ -31,9 +61,9 @@
 
                 <tbody>
 
-                        {
+                    {
                         posts.map(post => (
-                            <tr key={"kol_"+post.id}>
+                            <tr key={"kol_" + post.id}>
                                 <td>{post.street}</td>
                                 <td>{new Date(post.startDate).toLocaleString("pl-PL", { weekday: "long", year: "2-digit", month: "2-digit", day: "2-digit", hour: "numeric", minute: "2-digit" })}</td>
                                 <td>{new Date(post.endDate).toLocaleString("pl-PL", { weekday: "long", year: "2-digit", month: "2-digit", day: "2-digit", hour: "numeric", minute: "2-digit" })}</td>
@@ -41,10 +71,13 @@
                                 <td>{post.minName}</td>
                                 <td>{post.minTel}</td>
                             </tr>
-                            ))
-                        }
+                        ))
+                    }
 
                 </tbody>
             </table >
         </div >
     );
+};
+
+export default DataFetching;
