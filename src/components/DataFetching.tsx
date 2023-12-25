4642e6639d6336c33dbebde486f491a0b3aa7269
Michael Mleczek <michalmleczek@outlook.com>
2023-12-25 19:29:27 +0100

    all

--- src/components/DataFetching.tsx
+++ src/components/DataFetching.tsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 interface Post {
-    street: string;
+    id: string;
+    title: string;
 }
 
 const DataFetching: React.FC = () => {
     const [posts, setPosts] = useState<Post[]>([]);
 
     useEffect(() => {
-        axios.get<Post[]>('https://probewebapp20231222201703.azurewebsites.net/api/minkol/list?id=1')
+        axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts') 
             .then(res => {
                 console.log(res);
                 setPosts(res.data);
@@ -19,12 +20,14 @@ const DataFetching: React.FC = () => {
             });
     }, []);
 
+    // https://probewebapp20231222201703.azurewebsites.net/api/minkol/list?id=1
+
     return (
         <div>
             <ul>
                 {
                     posts.map(post => (
-                        <li key={post.street} > {post.street} </li>
+                        <li key={post.id} > {post.title} </li>
                     ))
                 }
             </ul>
