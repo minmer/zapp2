100f8870825c8d418fb2c06d522c31f854f6b2d4
Michael Mleczek <michalmleczek@outlook.com>
2023-12-26 09:49:39 +0100

    all

--- src/components/DateFetching.tsx
+++ src/components/DateFetching.tsx
@@ -30,6 +30,7 @@ const DateFetching: React.FC<Props> = (region) => {
     };
     useEffect(() => {
         if (region.id != -1) {
+            console.log(region.id);
             axios.get<Street[]>('https://probewebapp20231222201703.azurewebsites.net/api/minkol/region?id=' + region)
                 .then(res => {
                     console.log(res);
