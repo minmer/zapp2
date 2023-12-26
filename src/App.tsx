ba1d969a0487e6f9b0b4d9bf7945074869d3dfc0
Michael Mleczek <michalmleczek@outlook.com>
2023-12-26 21:31:19 +0100

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import DataFetching from "./components/DataFetching"
+import PrFetching from "./components/PrFetching"
 import './App.css'
 
 function App() {
     const queryParameters = new URLSearchParams(window.location.search)
     const kol = parseInt(queryParameters.get("kol") || "-1")
+    const pr = parseInt(queryParameters.get("pr") || "-1")
     const cod = parseInt(queryParameters.get("cod") || "-1")
 
 
@@ -12,7 +14,9 @@ function App() {
             <div>
               {kol != -1 ? (
                   <DataFetching key={kol} id={kol} cod={cod} />
-          ) : (
+              ) : pr != -1 ? (
+                  <PrFetching key={kol} id={kol} cod={cod} />
+              ) : (
               <h1>Hello World!</h1>
           )}
       </div>
---
+++ src/components/PrFetching.tsx
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DateFetching from './DateFetching';
+
+interface Post {
+    id: number,
+    startDate: Date,
+    endDate: Date,
+    min1Name: string,
+    min1Tel: string,
+    min2Name: string,
+    min2Tel: string,
+    priest: string,
+    street: string
+}
+interface Props {
+    id: number,
+    cod: number,
+}
+
+const PrFetching: React.FC<Props> = (kol) => {
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [koleda, setKoleda] = useState(-1);
+
+
+
+    const handleClick = (id:number) => {
+        setKoleda(id)
+    }
+
+    useEffect(() => {
+        axios.get<Post[]>('https://probewebapp20231222201703.azurewebsites.net/api/minkol/pr?id=' + kol.id + '&cod=' + kol.cod)
+            .then(res => {
+                console.log(res);
+                setPosts(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
+    // style={{border-collapse: "collapse"}}
+    return (
+
+        <div>
+            <h2>Kolęda 2023/24</h2>
+
+            <br />
+            W rozpisce kolędy zostało uwzględnione następujące rzeczy:
+            <br />
+            1. Ilość punktów wynikających ze służenia
+            < br />
+            2. Staż w służbie liturgicznej(nowi ministranci, ministranci, lektorzy)
+            < br />
+            3. Terminy zgłoszone w formularzu
+            < br />
+            4. Miejsce zamieszkania(w dniu, w którym macie kolędę, mam nadzieję, że będziecie dostępni)
+            < br />
+            Przy rozpisaniu kolędy nie miałem na bieżąco przed sobą listę z imionami, więc przepraszam, jeśli zapomniałem uwzględnić jakieś osobiste prośby.
+            < br />
+            <h1 id="name"></h1>
+            <br />
+            <table>
+                <thead>
+                    <tr>
+                        <th>Rejon/ulice</th>
+                        <th>Rozpoczęcie</th>
+                        <th>Zakończenie</th>
+                        <th>Ksiądz</th>
+                        <th>1. ministranta</th>
+                        <th>Telefon 1. ministranta</th>
+                        <th>2. ministranta</th>
+                        <th>Telefon 2. ministranta</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    {
+                        posts.map(post => (
+                            <tr key={"kol_" + post.id}>
+                                <td>{post.street}</td>
+                                <td>{new Date(post.startDate).toLocaleString("pl-PL", { weekday: "long", year: "2-digit", month: "2-digit", day: "2-digit", hour: "numeric", minute: "2-digit" })}</td>
+                                <td>{new Date(post.endDate).toLocaleString("pl-PL", { weekday: "long", year: "2-digit", month: "2-digit", day: "2-digit", hour: "numeric", minute: "2-digit" })}</td>
+                                <td>{post.priest}</td>
+                                <td>{post.min1Name}</td>
+                                <td>{post.min1Tel}</td>
+                                <td>{post.min2Name}</td>
+                                <td>{post.min2Tel}</td>
+                                <td><button onClick={() => handleClick(post.id)}>Szczegóły</button></td>
+                            </tr>
+                        ))
+                    }
+
+                </tbody>
+            </table >
+            <DateFetching id={koleda} min={kol.id} cod={kol.cod} key={"koleda_" + koleda} />
+        </div >
+    );
+};
+
+export default PrFetching;
